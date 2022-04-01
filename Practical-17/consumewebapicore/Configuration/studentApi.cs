759e922963f4bed2e79f920a5fc44fc5d5569841
Sweta-20 <97084227+Sweta-20@users.noreply.github.com>
2022-04-01 10:44:03 +0530

    updated

--- Practical-17/consumewebapicore/Configuration/studentApi.cs
+++ Practical-17/consumewebapicore/Configuration/studentApi.cs
@@ -11,7 +11,7 @@ namespace consumewebapicore.Configuration
         public HttpClient initial()
         {
             var client = new HttpClient();
-            client.BaseAddress = new Uri("http://localhost:54609/");
+            client.BaseAddress = new Uri("https://localhost:44306/");
             return client;
         }
     }
